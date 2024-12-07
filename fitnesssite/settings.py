bcd126671f0e471a9133c46d7ebe11f43360c068
DianaHosh <146379540+DianaHosh@users.noreply.github.com>
2024-12-07 23:07:28 +0200

    Update settings.py

--- fitnesssite/settings.py
+++ fitnesssite/settings.py
@@ -20,7 +20,7 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'login'
 
-
+ENABLE_ORYX_BUILD = true
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
